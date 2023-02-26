dc9800a63df6f5a9189581291fc438249cace162
Neobraingit <maervca@icloud.com>
2023-02-26 09:26:37 +0100

    modulos

---
+++ 12_modulos.py
@@ -0,0 +1,18 @@
+### MÓDULOS ###
+
+from module import sum, mifuncion
+
+
+#from 09_funciones import sum_two_values() # Esto da error porque el fichero importado empieza por un número
+
+sum(3,4,5)
+mifuncion()
+
+import math
+print (math.pi)
+print (math.pow(2, 8))
+
+from math import pi
+print (pi)
+
+
---
+++ module.py
@@ -0,0 +1,7 @@
+def sum(num1, num2, num3):
+    print (num1 + num2 + num3)
+    
+sum (5,6,7)
+
+def mifuncion():
+    print ('Me llamo Marcos')
