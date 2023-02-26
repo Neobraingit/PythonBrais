760d297c975403451201a89221ea40306ab2c15d
Neobraingit <maervca@icloud.com>
2023-02-26 17:40:33 +0100

    funciones_orden_superior

---
+++ 17_funciones_orden_superior.py
@@ -0,0 +1,12 @@
+### FUNCIONES DE ORDEN SUPERIOR ###
+
+def sun_one(value):
+    return value + 1
+
+def sum_two_values_and_add_one(first_value, second_value):
+    return sun_one(first_value, second_value)
+
+print (sum_two_values_and_add_one(5,2))   
+
+
+    
