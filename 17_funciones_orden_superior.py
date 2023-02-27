874dc3e994503a41486ae57366f693e043f40ba1
Neobraingit <Maervca2@icloud.com>
2023-02-27 21:52:47 +0100

    Probando pycharm

--- 17_funciones_orden_superior.py
+++ 17_funciones_orden_superior.py
@@ -6,7 +6,9 @@ def sun_one(value):
 def sum_two_values_and_add_one(first_value, second_value):
     return sun_one(first_value, second_value)
 
-print (sum_two_values_and_add_one(5,2))   
+print (sum_two_values_and_add_one(5,2))
+
+print ('Hola mundo')
 
 
     
