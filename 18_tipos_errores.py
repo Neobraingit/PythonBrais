d415657bb1bb079e725e16e89a0b103ee1f444f4
Neobraingit <Maervca2@icloud.com>
2023-03-01 16:37:27 +0100

    tipos de errores

---
+++ 18_tipos_errores.py
@@ -0,0 +1,36 @@
+### TIPOS DE ERRORES ###
+
+# SyntaxError
+
+print 'Hola comunidad' # Nos faltan los paréntesis
+
+
+# NameError
+
+print (variable) # Variable is not defined por que no está definida variable
+
+
+# IndexError
+
+lenguajesdeprogramación = 'Python', 'Swift', 'Php'
+print (lenguajesdeprogramación[5])
+
+# ModuleNotFoundError
+
+import modulo # No encuentra el módulo
+
+# AttributeError
+
+print (math.PI) # Nos devuelve el error de atributo no encontrado
+
+# KeyError
+
+my_dict = {'nombre' : 'Marcos', 'edad' : 48}
+print (my_dict['Marco'])  # Nos da un error de llave
+
+
+
+
+
+
+
